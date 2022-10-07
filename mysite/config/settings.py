cd2d238b9516f3c68d34544fb33fa779cdb10fd7
miruy <dbflarla4966@naver.com>
2022-10-07 17:00:18 +0900

    django 기본 게시판 구현

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [    # STATICFILES_DIRS 라는 리스트 변수 추가
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
