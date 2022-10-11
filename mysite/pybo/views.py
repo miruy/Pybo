dda1777a5b145eefe26198f5cc785fd4a3329cca
miruy <dbflarla4966@naver.com>
2022-10-12 00:51:14 +0900

    django 게시판 페이징처리 추가(Paginator)

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -6,11 +6,21 @@ from .models import Question    # model 중 Question 모델 사용
 from django.utils import timezone
 from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지 : http://localhost:8000/pybo/?page=1 처럼 GET 방식으로 호출된 URL에서 page값을 가져올 때 사용하는데 메인페이지로 호출됫을 시 기본값으로 page를 1로 설정
     question_list = Question.objects.order_by('-create_date')   # order_by() : 조회 결과를 정렬하는 함수, -가 있으면 역방향/없으면 순방향
-    context = {'question_list' : question_list} #
+    paginator = Paginator(question_list, 10)    # 페이지 당 10개씩 보여주기
+    page_obj = paginator.get_page(page)  # 요청된 페이지에 해당되는 페이징 객체 생성 -> 페이징 객체에는 다양한 속성 존재(블로그 참고)
+    last_page_num = 0
+    for last_page in paginator.page_range:
+        last_page_num = last_page
+    context = {
+        'question_list': page_obj,      # question_list는 페이징 객체
+        'last_page_num': last_page_num,   # 마지막 페이지
+    }
     return render(request, 'pybo/question_list.html', context) # 조회된 질문목록을 pybo/question_list.html 템플릿 파일로 리턴하여 응답
 
 
