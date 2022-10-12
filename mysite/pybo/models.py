9392142037bf88af6982c341ed4cb18e8ab10636
miruy <dbflarla4966@naver.com>
2022-10-12 22:22:34 +0900

    django 게시판 작성자 추가

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here. DB에 필요한 model 생성, ORM으로 간편하게 사용가능
 
+
 class Question(models.Model):   # 질문 클래스 생성
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)  # 제목 컬럼, 글자수 길이 제한이 필요한 속성은 CharField()사용
     content = models.TextField()    # 내용 컬럼, 글자수 길이 제한 필요없는 속성은 TextField()사용
     create_date = models.DateTimeField()    # 작성일
@@ -10,7 +12,9 @@ class Question(models.Model):   # 질문 클래스 생성
     def __str__(self):
         return self.subject
 
+
 class Answer(models.Model): # 답변 클래스 생성
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)    # 질문의 대한 답변이므로 Question클래스(테이블)을 참조해야함, 질문과 답변이 이어진 데이터가 삭제될 경우 함께 삭제(하나의 질문에 많은 답변이 달릴 수 있으므로)
     content = models.TextField()
     create_date = models.DateTimeField()
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect # render : 파이썬 데이터를 템플릿에 적용하여 html로 반환하는 함수
 from .models import Question    # model 중 Question 모델 사용
 from django.utils import timezone
-from django.http import HttpResponseNotAllowed
+# from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -30,30 +31,32 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')      # 로그인이 필요한 함수라는 것을 정의
 def answer_create(request, question_id):
-    """
-    pybo 답변등록
-    """
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user    # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
+        # return HttpResponseNotAllowed('Only POST is possible.')
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')       # 로그인이 필요한 함수라는 것을 정의
 def question_create(request):   # 질문 등록 메서드
     if request.method == 'POST':    # post로 요청이 왔다면
         form = QuestionForm(request.POST)   # 요청이 온 객체를 form이라는 변수에 대입
         if form.is_valid():   # 요청이 온 객체가 데이터를 가지고 있다면(질문제목과 내용이 있다면)
             question = form.save(commit=False)  # 질문제목, 내용 데이터 임시 저장(아직 DB에는 저장안함)
+            question.author = request.user      # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()   # 작성일 저장
             question.save()  # 데이터를 실제로 객체에 저장(실제 DB에 데이터 저장)
             return redirect('pybo:index')   # 성공 후 질문 목록페이지로 리턴
